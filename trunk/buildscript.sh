b869d5b9055437f012e7c14c5714de71c04a225a
rick.falkvinge <rick.falkvinge@f2b24750-7dd2-4bb6-9ed1-6ee67c2144e7>
2011-05-18 12:13:26 +0000

    Testing - this build should fail in CCNET

--- trunk/buildscript.sh
+++ trunk/buildscript.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #
 
-xbuild /logger:/opt/ccnet/Rodemeyer.MsBuildToCCnet.dll
-cp msbuild-output.xml ../artifact/msbuild-results.xml
+if xbuild /logger:/opt/ccnet/Rodemeyer.MsBuildToCCnet.dll; then
+	echo "Build Successful"
+else
+	echo "Build Failed; Exiting"
+	exit 1
+fi
 
 
