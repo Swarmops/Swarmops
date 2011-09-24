98a7fb55d964736593a517f03558d261659885e3
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2011-09-24 03:16:17 +0000

    Testing locatization #2 (this was problematic before)

--- Site5/Default.aspx.cs
+++ Site5/Default.aspx.cs
@@ -9,6 +9,6 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        LabelTranslation2.Text = Resources.Pages.Global.CurrentUserInfo_SelectLanguage;
     }
 }
