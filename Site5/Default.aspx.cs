da9891bb91395bd086d2829cfd11359834ea579f
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2011-12-07 20:48:29 +0000

    Database test

--- Site5/Default.aspx.cs
+++ Site5/Default.aspx.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Activizr.Logic.Structure;
 
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        LabelOrgOne.Text = Organization.FromIdentity(1).Name;
         LabelTranslation2.Text = Resources.Pages.Global.CurrentUserInfo_SelectLanguage;
     }
 }
