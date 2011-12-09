4dc5884873550a573a02ab44185903dbbd026c86
rick.falkvinge <rick.falkvinge@f2b24750-7dd2-4bb6-9ed1-6ee67c2144e7>
2011-12-09 01:12:33 +0000

    Fix

--- trunk/Site5/Security/Login.aspx.cs
+++ trunk/Site5/Security/Login.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class Security_Default : System.Web.UI.Page
+public partial class Security_Login : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
