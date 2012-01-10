a73d9618affaef88671f69d186ce095df01248a7
rick.falkvinge <rick.falkvinge@f2b24750-7dd2-4bb6-9ed1-6ee67c2144e7>
2012-01-10 04:46:49 +0000

    Committing bookkeeping integrity checks

---
+++ trunk/Site5/Tests/TestBookkeepingIntegrity.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master-v5.master" AutoEventWireup="true" CodeFile="TestBookkeepingIntegrity.aspx.cs" Inherits="Tests_TestBookkeepingIntegrity" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="PlaceHolderHead" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="PlaceHolderMain" Runat="Server">
+<br/>
+<div class="entrylabels" style="width:150px">Organization<br/>Year<br/>&nbsp;<br/>EOY Balance<br/>EOLY Owncapital<br/>EOCY Owncapital<br/>Diff<br/>Actual OC Delta<br/>Results-all<br/>Results-noted</div>
+<div class="entryfields">
+    <asp:Label ID="LabelThisOrganization" runat="server" /><br/>
+    <asp:DropDownList runat="server" ID="DropYear" AutoPostBack="true" OnSelectedIndexChanged="DropYear_SelectedIndexChanged" />&nbsp;<br/>&nbsp;<br/>
+    <asp:Label ID="LabelEoyBalance" runat="server" /><br/>
+    <asp:Label ID="LabelEolyOwnCapital" runat="server" /><br/>
+    <asp:Label ID="LabelEocyOwnCapital" runat="server" /><br/>
+    <asp:Label ID="LabelOwnCapitalDiff" runat="server" /><br/>
+    <asp:Label ID="LabelOwnCapitalDelta" runat="server" /><br/>
+    <asp:Label ID="LabelResultsAll" runat="server" /><br/>
+    <asp:Label ID="LabelResultsNoted" runat="server" />
+</div>
+
+<div style="clear:both"></div>
+
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="PlaceHolderSide" Runat="Server">
+    <h2 class="blue">INFO<span class="arrow"></span></h2>
+    
+    <div class="box">
+        <div class="content">
+            This page is for diagnostic tests of the integrity of the Activizr system. It is not intended for everyday use and is not accessible from the menu.
+        </div>
+    </div>
+    
+</asp:Content>
+
---
+++ trunk/Site5/Tests/TestBookkeepingIntegrity.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Activizr.Basic.Enums;
+using Activizr.Logic.Financial;
+
+public partial class Tests_TestBookkeepingIntegrity : PageV5Base
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        this.PageTitle = "Test Bookkeeping Integrity";
+        this.PageIcon = "iconshock-tester";
+
+        if (!Page.IsPostBack)
+        {
+            this.DropYear.Items.Add(new ListItem("--Select--", "0"));
+            this.DropYear.Items.Add(new ListItem("2008", "2008"));
+            this.DropYear.Items.Add(new ListItem("2009", "2009"));
+            this.DropYear.Items.Add(new ListItem("2010", "2010"));
+            this.DropYear.Items.Add(new ListItem("2011", "2011"));
+        }
+
+        this.LabelThisOrganization.Text = _currentOrganization.Name;
+    }
+
+    protected void DropYear_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        int year = Int32.Parse(this.DropYear.SelectedValue);
+
+        if (year < 2000) return;
+
+        FinancialAccounts balanceAccounts = FinancialAccounts.ForOrganization(_currentOrganization,
+                                                                              FinancialAccountType.Balance);
+
+        FinancialAccounts resultAccounts = FinancialAccounts.ForOrganization(_currentOrganization,
+                                                                             FinancialAccountType.Result);
+
+        FinancialAccount ownCapital = _currentOrganization.FinancialAccounts.DebtsCapital;
+        FinancialAccount resultAsNoted = _currentOrganization.FinancialAccounts.CostsYearResult;
+
+        FinancialAccounts balancesWithoutCapital =
+            FinancialAccounts.ForOrganization(_currentOrganization, FinancialAccountType.Balance);
+        balancesWithoutCapital.Remove(ownCapital);
+
+        FinancialAccounts resultAccountsWithoutNotedResult = FinancialAccounts.ForOrganization(_currentOrganization,
+                                                                             FinancialAccountType.Result);
+        resultAccountsWithoutNotedResult.Remove(_currentOrganization.FinancialAccounts.CostsYearResult);
+
+        Currency currency = _currentOrganization.DefaultCountry.Currency;
+
+        this.LabelResultsAll.Text = String.Format("{0} {1:N2}", currency.Code,
+                                                  resultAccounts.GetDeltaCents(new DateTime(year, 1, 1),
+                                                                               new DateTime(year + 1, 1, 1)) / 100.0);
+
+        this.LabelResultsNoted.Text = String.Format("{0} {1:N2}", currency.Code,
+                                                    resultAsNoted.GetDeltaCents(new DateTime(year, 1, 1),
+                                                                                new DateTime(year + 1, 1, 1))/100.0);
+
+        this.LabelEoyBalance.Text = String.Format("{0} {1:N2}", currency.Code,
+                                                    balanceAccounts.GetDeltaCents(new DateTime(1900, 1, 1),
+                                                                                new DateTime(year + 1, 1, 1)) / 100.0);
+
+        Int64 endOfLastYearCapital = ownCapital.GetDeltaCents(new DateTime(1900, 1, 1),
+                                                              new DateTime(year, 1, 1));
+
+        Int64 endOfSelectedYearCapital = ownCapital.GetDeltaCents(new DateTime(1900, 1, 1),
+                                                              new DateTime(year + 1, 1, 1));
+
+        this.LabelEolyOwnCapital.Text = String.Format("{0} {1:N2}", currency.Code, endOfLastYearCapital / 100.0);
+
+        this.LabelEocyOwnCapital.Text = String.Format("{0} {1:N2}", currency.Code,
+                                            endOfSelectedYearCapital / 100.0);
+
+        this.LabelOwnCapitalDiff.Text = String.Format("{0} {1:N2}", currency.Code,
+                                            (endOfSelectedYearCapital - endOfLastYearCapital) / 100.0);
+
+        this.LabelOwnCapitalDelta.Text = String.Format("{0} {1:N2}", currency.Code,
+                                            ownCapital.GetDeltaCents(new DateTime(year, 1, 1),
+                                                                        new DateTime(year + 1, 1, 1)) / 100.0);
+
+    }
+}
