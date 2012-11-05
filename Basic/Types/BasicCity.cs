6af0345ebc736a9864de576eb81522efd9c614e3
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2012-11-05 12:02:10 +0000

    Added default ctors to more classes

--- Basic/Types/BasicCity.cs
+++ Basic/Types/BasicCity.cs
@@ -5,6 +5,7 @@ using Activizr.Basic.Interfaces;
 
 namespace Activizr.Basic.Types
 {
+    [Serializable]
     public class BasicCity : IHasIdentity
     {
         public BasicCity (int cityId, string name, int countryId, int geographyId)
@@ -18,6 +19,12 @@ namespace Activizr.Basic.Types
         public BasicCity (BasicCity original)
             : this(original.cityId, original.name, original.countryId, original.geographyId)
         {
+        }
+
+        [Obsolete ("Do not call this ctor directly. It exists to enable serialization.", true)]
+        public BasicCity()
+        {
+            // this ctor should never be called from code
         }
 
         private int cityId;
