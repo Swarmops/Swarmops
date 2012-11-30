de1cb60e25e1ae6864dff8bb04f0f8261937443a
rick.falkvinge <rick.falkvinge@f2b24750-7dd2-4bb6-9ed1-6ee67c2144e7>
2012-11-30 01:25:09 +0000

    Committing various work - mostly icons; started on Request Cash Advance

---
+++ trunk/Site5/Pages/v5/Finance/RequestCashAdvance.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Pages_v5_Finance_RequestCashAdvance : PageV5Base
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        this.PageTitle = Resources.Pages.Finance.RequestCashAdvance_PageTitle;
+        this.PageIcon = "iconshock-walletmoney";
+    }
+}
