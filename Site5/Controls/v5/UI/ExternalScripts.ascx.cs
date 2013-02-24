80898dde3108fc42b4095d7341a02db8c768a328
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2013-02-24 18:33:01 +0000

    Testing external scripts on dev deployment. If works, deploying stable

---
+++ Site5/Controls/v5/UI/ExternalScripts.ascx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Swarmops.Frontend.Controls.v5.UI
+{
+    public partial class ExternalScripts : ControlV5Base
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string externalScriptUrl = "//hostedscripts.falkvinge.net";
+
+            string testFolderName = Server.MapPath("~/Scripts/ExternalScripts");
+            if (Directory.Exists(testFolderName))
+            {
+                externalScriptUrl = "/Scripts/ExternalScripts";
+            }
+
+            if (Package == "easyui")
+            {
+                this.LiteralReference1.Text = "<script src=\"" + externalScriptUrl +
+                                        "/easyui/jquery.easyui.min.js\" type=\"text/javascript\"></script>";
+                this.LiteralReference2.Text = "<link rel=\"stylesheet\" type=\"text/css\" href=\"" + externalScriptUrl +
+                                        "/easyui/themes/icon.css\" />";
+                this.LiteralReference3.Text = "<link rel=\"stylesheet\" type=\"text/css\" href=\"" + externalScriptUrl +
+                                        "/easyui/themes/default/" + this.Control + ".css\" />";
+            }
+        }
+
+        public string Package { get; set; }
+        public string Control { get; set; }
+    }
+}
