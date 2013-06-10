8946012a8a9288a553c0895ca2208b9f23d655c2
rick.falkvinge <rick.falkvinge@f2b24750-7dd2-4bb6-9ed1-6ee67c2144e7>
2013-06-10 20:32:42 +0000

    Checking in types for BasicOutboundComms / -Recipients

---
+++ trunk/Basic/Types/Communications/BasicOutboundComm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Swarmops.Basic.Enums;
+using Swarmops.Basic.Interfaces;
+
+namespace Swarmops.Basic.Types.Communications
+{
+    public class BasicOutboundComm: IHasIdentity
+    {
+        public BasicOutboundComm (int outboundCommId, int senderPersonId, int organizationId, DateTime createdDateTime, string resolverClass, string recipientDataXml, bool resolved, DateTime resolvedDateTime, OutboundCommPriority priority, string transmitterClass, string payloadXml,
+            bool open, DateTime startTransmitDateTime, DateTime closedDateTime, int recipientCount, int recipientsSuccess, int recipientsFail)
+        {
+            this.OutboundCommId = outboundCommId;
+            this.SenderPersonId = senderPersonId; // should we include a "fromPersonId" in basedata too, when deputies are sending on behalf of avatars?
+            this.OrganizationId = organizationId;
+            this.CreatedDateTime = createdDateTime;
+            this.ResolverClass = resolverClass;
+            this.RecipientDataXml = recipientDataXml;
+            this.Resolved = resolved;
+            this.ResolvedDateTime = resolvedDateTime;
+            this.Priority = priority;
+            this.TransmitterClass = transmitterClass;
+            this.PayloadXml = payloadXml;
+            this.Open = open;
+            this.StartTransmitDateTime = startTransmitDateTime;
+            this.ClosedDateTime = closedDateTime;
+            this.RecipientCount = recipientCount;
+            this.RecipientsSuccess = recipientsSuccess;
+            this.RecipeintsFail = recipientsFail;
+        }
+
+
+        public BasicOutboundComm (BasicOutboundComm original) :
+            this (original.OutboundCommId, original.SenderPersonId, original.OrganizationId, original.CreatedDateTime, original.ResolverClass, original.RecipientDataXml, original.Resolved, original.ResolvedDateTime, original.Priority, original.TransmitterClass, original.PayloadXml,
+            original.Open, original.StartTransmitDateTime, original.ClosedDateTime, original.RecipientCount, original.RecipientsSuccess, original.RecipeintsFail)
+        {
+            // copy ctor    
+        }
+
+
+
+        public int OutboundCommId { get; private set; }
+        public int SenderPersonId { get; private set; }
+        public int OrganizationId { get; private set; }
+        public DateTime CreatedDateTime { get; private set; }
+        public string ResolverClass { get; private set; }
+        public string RecipientDataXml { get; private set; }
+        public bool Resolved { get; protected set; }
+        public DateTime ResolvedDateTime { get; protected set; }
+        public OutboundCommPriority Priority { get; private set; }
+        public string TransmitterClass { get; private set; }
+        public string PayloadXml { get; private set; }
+        public bool Open { get; protected set; }
+        public DateTime StartTransmitDateTime { get; protected set; }
+        public DateTime ClosedDateTime { get; protected set; }
+        public int RecipientCount { get; protected set; }
+        public int RecipientsSuccess { get; protected set; }
+        public int RecipeintsFail { get; protected set; }
+
+
+
+        #region Implementation of IHasIdentity
+
+        public int Identity
+        {
+            get { return this.OutboundCommId; }
+        }
+
+        #endregion
+    }
+}
---
+++ trunk/Basic/Types/Communications/BasicOutboundCommRecipient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Swarmops.Basic.Interfaces;
+
+namespace Swarmops.Basic.Types.Communications
+{
+    public class BasicOutboundCommRecipient: IHasIdentity
+    {
+        public BasicOutboundCommRecipient (int outboundCommRecipientId, int outboundCommId, int personId, bool open, bool success, string failReason)
+        {
+            this.OutboundCommRecipientId = outboundCommRecipientId;
+            this.OutboundCommId = outboundCommId;
+            this.PersonId = personId;
+            this.Open = open;
+            this.Success = success;
+            this.FailReason = failReason;
+        }
+
+        public int OutboundCommRecipientId { get; private set; }
+        public int OutboundCommId { get; private set; }
+        public int PersonId { get; private set; }
+        public bool Open { get; protected set; }
+        public bool Success { get; protected set; }
+        public string FailReason { get; protected set; }
+
+        #region Implementation of IHasIdentity
+
+        public int Identity
+        {
+            get { return this.OutboundCommRecipientId; }
+        }
+
+        #endregion
+    }
+}
