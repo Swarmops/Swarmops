d8b302edb24ab23ba38b29dd19cbe5ab8436e7cc
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2013-09-22 21:19:24 +0000

    minor refactoring into custom controls

---
+++ Site5/Controls/v5/Financial/ComboIncomeCostType.ascx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Controls_v5_Financial_ComboIncomeCostType : System.Web.UI.UserControl
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
