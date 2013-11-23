f1fcae9dfe80c02058115f6202e2685de30fee06
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2013-11-23 18:04:56 +0000

    Fixed important force-HTTPS bug

--- Site5/Master-v5.master.cs
+++ Site5/Master-v5.master.cs
@@ -62,9 +62,9 @@ namespace Swarmops
 
             // Check for SSL and force it
 
-            if (!Request.IsSecureConnection)
+            if (!Request.IsSecureConnection || Request.Url.ToString().StartsWith("http://"))  // Both are necessary, as some reverse proxies will https-ify an original nonsecure connection
             {
-                if (!Request.Url.ToString().StartsWith("http://dev.swarmops.com/") && !Request.Url.ToString().StartsWith("http://live.activizr.com") && !Request.Url.ToString().StartsWith("http://localhost:") && !Request.Url.ToString().StartsWith("http://cryptdb"))
+                if (!Request.Url.ToString().StartsWith("http://dev.swarmops.com/") && !Request.Url.ToString().StartsWith("http://sandbox.swarmops.com") && !Request.Url.ToString().StartsWith("http://localhost:") && !Request.Url.ToString().StartsWith("http://cryptdb"))
                 {
                     // Response.Redirect(Request.Url.ToString().Replace("http:", "https:"));
 
