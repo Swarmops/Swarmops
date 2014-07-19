4e23f2cc79dd4b5a923a81598503913215befa33
rick.falkvinge <rick.falkvinge@f2b24750-7dd2-4bb6-9ed1-6ee67c2144e7>
2014-07-19 08:18:40 +0000

    Committing some work

---
+++ trunk/Site5/Controls/v5/Base/MainMenu.ascx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Swarmops.Controls.Base
+{
+    public partial class MainMenu : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public MainMenuItem[] MainMenuData { get; set; }
+    }
+
+    [Serializable]
+    public class MainMenuItem
+    {
+        public string ResourceString;
+        public string IconUrl;
+        public string NavigateUrl;
+        public int UserLevel;
+        public string Permission;
+        public bool IsSeparator;
+        public MainMenuItem[] Children;
+    }
+}
