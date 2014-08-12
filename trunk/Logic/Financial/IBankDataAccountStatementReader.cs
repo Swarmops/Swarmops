7a432f99a54e121f0feaf1806aeb6e19909fb4c7
rick.falkvinge <rick.falkvinge@f2b24750-7dd2-4bb6-9ed1-6ee67c2144e7>
2014-08-12 20:45:18 +0000

    Committing some work. Build is still broken. Calling it a night.

---
+++ trunk/Logic/Financial/IBankDataAccountStatementReader.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using Swarmops.Logic.Financial.BankDataReaders;
+
+namespace Swarmops.Logic.Financial
+{
+    interface IBankDataAccountStatementReader
+    {
+        ImportedBankData ReadData(StreamReader data, FinancialAccount account, ExternalBankDataProfile profile);
+    }
+}
