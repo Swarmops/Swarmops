41ec895e82a6b08b92a7aeb6efa1d4dc968c8fbf
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2014-08-20 19:28:33 +0000

    Superfish is ugly as ugliest fuck BUT OPERATIONAL. Yay or something. Much work ahead to make it look decent...

--- Site5/Controls/v5/Base/MainMenu.ascx.cs
+++ Site5/Controls/v5/Base/MainMenu.ascx.cs
@@ -1,31 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml.Serialization;
 
+// ReSharper disable once CheckNamespace
 namespace Swarmops.Controls.Base
 {
-    public partial class MainMenu : System.Web.UI.UserControl
+    public partial class MainMenu : ControlV5Base
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // TODO: Put a small cache on this
 
+            // TODO: Put plugins on it
+
+            XmlSerializer serializer = new XmlSerializer(typeof(MainMenuItem[]));
+
+            using (TextReader reader = new StreamReader(Server.MapPath("~/MainMenu-v5.xml")))
+            {
+                this.MainMenuData = (MainMenuItem[]) serializer.Deserialize(reader);
+            }
+        }
+
+        protected override void Render(HtmlTextWriter output)
+        {
+            output.Write("<ul id='MainMenuContainer' class='sf-menu'>");
+            foreach (MainMenuItem menuItem in MainMenuData)
+            {
+                WriteMenuItem(menuItem, output);
+            }
+            output.Write("</ul>");
+        }
+
+        private void WriteMenuItem(MainMenuItem menuItem, HtmlTextWriter output)
+        {
+            output.Write("<li>");
+            string localizedText = "RESOURCE NOT FOUND";
+
+            if (!String.IsNullOrEmpty(menuItem.ResourceKey))
+            {
+                object resourceObject = GetGlobalResourceObject("Menu5", "Menu5_" + menuItem.ResourceKey);
+                if (resourceObject != null)
+                {
+                    localizedText = resourceObject.ToString();
+                }
+            }
+            localizedText = Server.HtmlEncode(localizedText); // muy importante
+
+            switch (menuItem.Type)
+            {
+                case MenuItemType.Link:
+                    output.Write("<img src=\"/Images/PageIcons/{0}-16px.png\" /><a href=\"{1}\">{2}</a>",
+                        menuItem.ImageUrl, menuItem.NavigateUrl, localizedText);
+                    break;
+                case MenuItemType.Disabled:
+                    string imageUrl = "/Images/PageIcons/" + menuItem.ImageUrl + "-16px.png";
+                    if (String.IsNullOrEmpty(menuItem.ImageUrl))
+                    {
+                        imageUrl = "/Images/PageIcons/transparency-16px.png";
+                    }
+                    output.Write("<img src=\"{0}\" />{1}", imageUrl, localizedText);
+                    break;
+                case MenuItemType.Submenu:
+                    output.Write(localizedText);
+                    break;
+                case MenuItemType.Separator:
+                    output.Write("---");
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            if (menuItem.Children.Length > 0)
+            {
+                output.Write("<ul>");
+                foreach (MainMenuItem child in menuItem.Children)
+                {
+                    WriteMenuItem(child, output);
+                }
+                output.Write("</ul>");
+            }
+
+            output.Write("</li>");
         }
 
         public MainMenuItem[] MainMenuData { get; set; }
-    }
-
-    [Serializable]
-    public class MainMenuItem
-    {
-        public string ResourceString;
-        public string IconUrl;
-        public string NavigateUrl;
-        public int UserLevel;
-        public string Permission;
-        public bool IsSeparator;
-        public MainMenuItem[] Children;
     }
 }
