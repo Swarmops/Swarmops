115c665aaad0a9f4ab67a6f8c2212f5e5ec9d613
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2014-08-26 05:49:04 +0000

    Added pilot installation ids

---
+++ Logic/Support/PilotInstallationIds.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Swarmops.Logic.Support
+{
+    public class PilotInstallationIds
+    {
+        static public string PiratePartySE
+        { get { return "d7588903-5fd0-40cf-a5b1-9af7a722cb6e"; } }
+    }
+}
