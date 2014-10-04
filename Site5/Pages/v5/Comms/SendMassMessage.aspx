f742f9c7b1dfab486c9494a6e15d331efb984c2b
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2014-10-04 20:10:20 +0200

    Checkpoint commit for SendMassMessage

---
+++ Site5/Pages/v5/Comms/SendMassMessage.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master-v5.master" AutoEventWireup="true" CodeFile="SendMassMessage.aspx.cs" Inherits="Swarmops.Frontend.Pages.Comms.SendMassMessage" %>
+<%@ Register src="~/Controls/v5/UI/ExternalScripts.ascx" tagname="ExternalScripts" tagprefix="Swarmops5" %>
+<%@ Register src="~/Controls/v5/Base/ComboGeographies.ascx" tagname="ComboGeographies" tagprefix="Swarmops5" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="PlaceHolderHead" Runat="Server">
+    <Swarmops5:ExternalScripts ID="ExternalScripts2" Package="easyui" Control="tree" runat="server" />
+	<link rel="stylesheet" type="text/css" href="/Style/v5-easyui-elements.css">
+    
+    <script type="text/javascript">
+
+        $(document).ready(function () {
+            // Document.Ready() goes here
+        });
+
+        var selectedReceipientClass = 1;
+        var selectedGeographyId = 1;  // TODO: SET ROOT GEOGRAPHY BY AUTHORITY/ACCESS
+
+    </script>
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="PlaceHolderMain" Runat="Server">
+    <div class="entryFields" style="padding-top:4px">
+        <asp:DropDownList runat="server" ID="DropRecipientClasses"/><br/>
+        <Swarmops5:ComboGeographies ID="ComboGeographies" runat="server" OnClientSelect="onGeographyChange" />&thinsp;<br/>
+    </div>
+    <div class="entryLabels" style="padding-top:10px">
+        <asp:Label ID="LabelRecipientType" runat="server" /><br/>
+        <asp:Label ID="LabelGeography" runat="server" /><br/>
+    </div>
+    <h2 style="padding-top:15px"><asp:Label ID="LabelHeaderMessage" runat="server" /> (<span id="spanRecipientCount">0</span>)</h2>
+    <asp:TextBox runat="server" TextMode="MultiLine" Rows="10" ID="TextMessage" />
+    <asp:Button runat="server" Text="Foo"/>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="PlaceHolderSide" Runat="Server">
+</asp:Content>
+
---
+++ Site5/Pages/v5/Comms/SendMassMessage.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Security;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Swarmops.Basic.Enums;
+using Swarmops.Database;
+using Swarmops.Logic.Financial;
+using Swarmops.Logic.Security;
+
+namespace Swarmops.Frontend.Pages.Comms
+{
+    public partial class SendMassMessage : PageV5Base
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.PageIcon = "iconshock-group-search";
+
+            if (!Page.IsPostBack)
+            {
+                Localize();
+            }
+
+            this.TextMessage.Style[HtmlTextWriterStyle.Width] = "674px";
+            this.TextMessage.Style["resize"] = "none";
+        }
+
+        private void Localize()
+        {
+            this.PageTitle = Resources.Pages.Comms.SendMassMessage_Title;
+            this.InfoBoxLiteral = Resources.Pages.People.ListFindPeople_Info;
+            this.LabelGeography.Text = Resources.Pages.Comms.SendMassMessage_Geography;
+            this.LabelRecipientType.Text = Resources.Pages.Comms.SendMassMessage_RecipientType;
+            this.LabelHeaderMessage.Text = Resources.Pages.Comms.SendMassMessage_HeaderMessage;
+
+            this.DropRecipientClasses.Items.Clear();
+            this.DropRecipientClasses.Items.Add(new ListItem(Resources.Global.Global_SelectOne));
+            this.DropRecipientClasses.Items.Add(new ListItem("[Regulars]", "1"));
+            this.DropRecipientClasses.Items.Add(new ListItem("[Agents]", "2"));
+            this.DropRecipientClasses.Items.Add(new ListItem("Officers", "3"));
+            this.DropRecipientClasses.Items.Add(new ListItem("Volunteers", "4"));
+        }
+
+        public struct ConfirmPayoutResult
+        {
+            public int AssignedId;
+            public string DisplayMessage;
+        };
+
+        [WebMethod]
+        public static ConfirmPayoutResult ConfirmPayout (string protoIdentity)
+        {
+            protoIdentity = HttpUtility.UrlDecode(protoIdentity);
+
+            AuthenticationData authData = GetAuthenticationDataAndCulture();
+
+            if (!authData.CurrentUser.HasAccess(new Access(authData.CurrentOrganization, AccessAspect.Financials, AccessType.Write)))
+            {
+                throw new SecurityAccessDeniedException("Insufficient privileges for operation");
+            }
+
+            ConfirmPayoutResult result = new ConfirmPayoutResult();
+
+            Payout payout = Payout.CreateFromProtoIdentity(authData.CurrentUser, protoIdentity);
+            Swarmops.Logic.Support.PWEvents.CreateEvent(EventSource.PirateWeb, EventType.PayoutCreated,
+                                                       authData.CurrentUser.Identity, 1, 1, 0, payout.Identity,
+                                                       protoIdentity);
+
+            // Create result and return it
+
+            result.AssignedId = payout.Identity;
+            result.DisplayMessage = String.Format(Resources.Pages.Financial.PayOutMoney_PayoutCreated, payout.Identity,
+                                                  payout.Recipient);
+
+            result.DisplayMessage = HttpUtility.UrlEncode(result.DisplayMessage).Replace("+", "%20");
+
+            return result;
+        }
+
+        public struct UndoPayoutResult
+        {
+            public bool Success;
+            public string DisplayMessage;
+        }
+
+        [WebMethod]
+        public static UndoPayoutResult UndoPayout (int databaseId)
+        {
+            AuthenticationData authData = GetAuthenticationDataAndCulture();
+            UndoPayoutResult result = new UndoPayoutResult();
+
+            Payout payout = Payout.FromIdentity(databaseId);
+
+            if (!payout.Open)
+            {
+                // this payout has already been settled, or picked up for settling
+
+                result.Success = false;
+                result.DisplayMessage = String.Format(Resources.Pages.Financial.PayOutMoney_PayoutCannotUndo,
+                                                      databaseId);
+
+                return result;
+            }
+
+            payout.UndoPayout();
+
+            result.DisplayMessage = HttpUtility.UrlEncode(String.Format(Resources.Pages.Financial.PayOutMoney_PayoutUndone, databaseId)).Replace("+","%20");
+            result.Success = true;
+            return result;
+        }
+    }
+
+}
