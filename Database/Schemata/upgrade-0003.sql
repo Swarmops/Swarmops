9188f2212cd3b25910c0a80dd59d0eee679c4083
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2014-10-21 06:47:17 +0200

    Added CreateSalaryTaxLevel procedure

--- Database/Schemata/upgrade-0003.sql
+++ Database/Schemata/upgrade-0003.sql
@@ -3,3 +3,26 @@ ADD COLUMN `Year` INT(11) NOT NULL DEFAULT 2014 AFTER `Tax`,
 ADD INDEX `Index_Year` (`Year` ASC)
 
 
+#
+
+
+CREATE PROCEDURE `CreateSalaryTaxLevel` (
+  taxLevelId INTEGER,
+  countryId INTEGER,
+  bracketLow INTEGER,
+  tax INTEGER,
+  year INTEGER
+)
+BEGIN
+
+  INSERT INTO SalaryTaxLevels (TaxLevelId,CountryId,BracketLow,Tax,Year)
+    VALUES (taxLevelId,countryId,bracketLow,tax,year);
+
+END
+
+
+#
+
+
+DROP PROCEDURE `CreateTaxLevel`
+
