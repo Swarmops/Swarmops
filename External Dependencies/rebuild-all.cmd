34ae1779065ad76fb62bac8cb81835c6e9f31108
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2014-10-29 11:44:01 +0100

    Silenced script a bit

--- External Dependencies/rebuild-all.cmd
+++ External Dependencies/rebuild-all.cmd
@@ -5,9 +5,9 @@ git submodule update
 git submodule foreach git checkout master
 git submodule foreach git pull
 
-msbuild /p:Configuration=Release /t:Clean;Build NBitcoin\NBitcoin\NBitcoin.csproj
+msbuild /p:Configuration=Release /t:Clean;Build NBitcoin\NBitcoin\NBitcoin.csproj > null
 
-if exist bin erase /s /q bin
+if exist bin erase /s /q bin > null
 if not exist bin mkdir bin
 cd bin
 xcopy /s ..\NBitcoin\NBitcoin\bin\Release\* .
