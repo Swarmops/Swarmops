058013db20a7837914b034b9a60476ca81f77743
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2014-10-30 15:44:14 +0100

    Adding NBitcoin to Logic, too

--- External Dependencies/rebuild-all.cmd
+++ External Dependencies/rebuild-all.cmd
@@ -5,9 +5,9 @@ git submodule update
 git submodule foreach git checkout master
 git submodule foreach git pull
 
-msbuild /p:Configuration=Release /t:Clean;Build NBitcoin\NBitcoin\NBitcoin.csproj > null
+msbuild /p:Configuration=Release /t:Clean;Build NBitcoin\NBitcoin\NBitcoin.csproj > nul
 
-if exist bin erase /s /q bin > null
+if exist bin erase /s /q bin > nul
 if not exist bin mkdir bin
 cd bin
 xcopy /s ..\NBitcoin\NBitcoin\bin\Release\* .
