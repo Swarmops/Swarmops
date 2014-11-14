063efeedb1ed4c61d2db1817f3283c657f8a4187
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2014-11-14 01:58:55 +0100

    Testing changes to build script

--- BuildFiles/Building/precompile-deploy.cmd
+++ BuildFiles/Building/precompile-deploy.cmd
@@ -1,7 +1,7 @@
 if not exist C:\Lab\Swarmops\build\Deploy-Staging\BuildDropped.txt goto testSprint
 cd \Lab\Swarmops\build\Deploy-Staging
-aspnet_compiler.exe -u -v / -p frontend-raw -f -fixednames -errorstack frontend
-aspnet_merge.exe -o Swarmops.Site.dll frontend
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\aspnet_compiler.exe -u -v / -p frontend-raw -f -fixednames -errorstack frontend
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\aspnet_merge.exe -o Swarmops.Site.dll frontend
 
 :testSprint
 echo ending
