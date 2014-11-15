dc07ef4733e43ddc0407716ebe47d195e811e7c4
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2014-11-15 07:03:29 +0100

    Change to build

--- External Dependencies/rebuild-all.cmd
+++ External Dependencies/rebuild-all.cmd
@@ -5,7 +5,7 @@ git submodule update
 git submodule foreach git checkout master
 git submodule foreach git pull
 
-msbuild /p:Configuration=Release;TargetFrameworkVersion=v4.5 /t:Clean;Build NBitcoin\NBitcoin\NBitcoin.csproj
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild /p:Configuration=Release;TargetFrameworkVersion=v4.5 /t:Clean;Build NBitcoin\NBitcoin\NBitcoin.csproj
 
 if exist bin erase /s /q bin > nul
 if not exist bin mkdir bin
