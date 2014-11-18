00486d97909325daf1d951c07c907d7e75363c95
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2014-11-18 22:02:57 +0100

    Fixed a stupid build error: bad 'using'

--- Site5/App_Code/CommonV5.cs
+++ Site5/App_Code/CommonV5.cs
@@ -3,7 +3,6 @@ using System.Drawing;
 using System.Globalization;
 using System.Threading;
 using System.Web;
-using Org.BouncyCastle.Asn1.Mozilla;
 using Swarmops.Logic.Structure;
 using Swarmops.Logic.Swarm;
 
