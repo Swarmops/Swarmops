23ddce82f07b2573eb2d4a475a845e22a361afa7
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2014-11-25 13:07:04 +0100

    Derpfix :(

--- Site5/Pages/v5/Financial/FileExpenseClaim.aspx.cs
+++ Site5/Pages/v5/Financial/FileExpenseClaim.aspx.cs
@@ -110,6 +110,7 @@ namespace Swarmops.Frontend.Pages.v5.Financial
                 Localize();
             }
 
+            EasyUIControlsUsed = EasyUIControl.Tree;
             IncludedControlsUsed = IncludedControl.FileUpload;
         }
 
