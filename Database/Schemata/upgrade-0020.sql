5968f0c361e71b2266f7dd2c50e4b1e76e0a02ba
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2015-04-16 21:51:14 +0200

    Fix to Db0020

--- Database/Schemata/upgrade-0020.sql
+++ Database/Schemata/upgrade-0020.sql
@@ -1,2 +1,27 @@
-ALTER TABLE `PositionAssignments` 
-ADD COLUMN `Acting` TINYINT NOT NULL DEFAULT '0' AFTER `CreatedByPositionId`;
+DROP procedure IF EXISTS `TerminatePositionAssignment`
+
+#
+
+CREATE PROCEDURE `TerminatePositionAssignment`(
+  IN positionAssignmentId INT,
+  IN terminatedDateTimeUtc DATETIME,
+  IN terminatedByPersonId INT,
+  IN terminatedByPositionId INT,
+  IN terminationNotes TEXT
+)
+BEGIN
+
+  UPDATE PositionAssignments
+    SET
+      PositionAssignments.Active=0, 
+      PositionAssignments.TerminatedDateTimeUtc=terminatedDateTimeUtc, 
+      PositionAssignments.TerminatedByPersonId=terminatedByPersonId,
+      PositionAssignments.TerminatedByPositionId=terminatedByPositionId,
+      PositionAssignments.TerminationNotes=terminationNotes
+    WHERE
+      PositionAssignments.PositionAssignmentId=positionAssignmentId AND
+      PositionAssignments.Active=1;
+      
+  SELECT ROW_COUNT() AS RowsUpdated;
+
+END
