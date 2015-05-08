116034bd2938ea59b20c11836e555994b931855d
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2015-05-08 12:52:02 +0200

    More work on Signup

--- Site5/Pages/v5/Public/Signup.aspx.cs
+++ Site5/Pages/v5/Public/Signup.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Swarmops.Logic.Structure;
 
 namespace Swarmops.Frontend.Pages.Public
 {
@@ -11,7 +12,20 @@ namespace Swarmops.Frontend.Pages.Public
     {
         protected void Page_Load (object sender, EventArgs e)
         {
+            this.Organization = Organization.FromVanityDomain (Request.Url.Host);
 
+            if (this.Organization == null)
+            {
+                int organizationId = Convert.ToInt32 (Request["OrganizationId"]); // may throw - not pretty but ok for now. // TODO
+                this.Organization = Organization.FromIdentity (organizationId); // may also throw, as above
+            }
+
+            if (this.Organization == null)
+            {
+                throw new ArgumentException("Organization not specified");  // TODO: make a friendly landing page instead
+            }
         }
+
+        protected Organization Organization;
     }
 }
