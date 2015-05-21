25ebf7f9c5225fbaf77152ab2db061bad21ad168
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2015-05-21 15:09:18 +0200

    Fixed missing reference

---
+++ Logic/Special/DummyGeoLookup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using NGeoIP;
+using NGeoIP.Client;
+
+namespace Swarmops.Logic.Special
+{
+    /// <summary>
+    /// The point of this class is to reference NGeoIP, so the DLL gets copied to the output folder for Site to use.
+    /// Kind of a kludgy hack, but works.
+    /// </summary>
+    public class DummyGeoLookup
+    {
+        public static string Lookup (string ip)
+        {
+            NGeoIP.Request request = new Request()
+            {
+                Format = Format.Json,
+                IP = HttpContext.Current.Request.UserHostAddress
+            };
+            NGeoClient client = new NGeoClient(request);
+            NGeoIP.RawData rawData = client.Execute();
+
+            return rawData.CountryCode;
+        }
+    }
+}
