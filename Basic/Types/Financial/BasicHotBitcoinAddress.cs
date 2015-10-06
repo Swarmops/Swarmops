d751b75b6a18131d9490d117c6e2070935cbbbbf
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2015-10-06 23:52:49 +0200

    Added Basic types for hotwallet addresses

---
+++ Basic/Types/Financial/BasicHotBitcoinAddress.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Swarmops.Common.Interfaces;
+
+namespace Swarmops.Basic.Types.Financial
+{
+    public class BasicHotBitcoinAddress: IHasIdentity
+    {
+        public BasicHotBitcoinAddress (int hotBitcoinAddressId, int organizationId, string derivationPath,
+            Int64 balanceSatoshis, Int64 throughputSatoshis)
+        {
+            this.HotBitcoinAddressId = hotBitcoinAddressId;
+            this.OrganizationId = organizationId;
+            this.DerivationPath = derivationPath;
+            this.BalanceSatoshis = balanceSatoshis;
+            this.ThroughputSatoshis = throughputSatoshis;
+        }
+
+        public BasicHotBitcoinAddress (BasicHotBitcoinAddress original)
+            : this (
+                original.HotBitcoinAddressId, original.OrganizationId, original.DerivationPath, original.BalanceSatoshis,
+                original.ThroughputSatoshis)
+        {
+            // copy ctor
+        }
+
+        public int HotBitcoinAddressId { get; private set; }
+        public int OrganizationId { get; private set; }
+        public string DerivationPath { get; private set; }
+        public Int64 BalanceSatoshis { get; protected set; }
+        public Int64 ThroughputSatoshis { get; protected set; }
+
+        public int Identity { get { return HotBitcoinAddressId; }}
+    }
+}
---
+++ Basic/Types/Financial/BasicHotBitcoinAddressUnspent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Swarmops.Common.Interfaces;
+
+namespace Swarmops.Basic.Types.Financial
+{
+    public class BasicHotBitcoinAddressUnspent: IHasIdentity
+    {
+        public BasicHotBitcoinAddressUnspent (int hotBitcoinAddressUnspentId, string transactionHash,
+            int transactionOutputIndex, Int64 amountSatoshis, int confirmationCount)
+        {
+            this.HotBitcoinAddressUnspentId = hotBitcoinAddressUnspentId;
+            this.TransactionHash = transactionHash;
+            this.TransactionOutputIndex = transactionOutputIndex;
+            this.AmountSatoshis = amountSatoshis;
+            this.ConfirmationCount = confirmationCount;
+        }
+
+        public BasicHotBitcoinAddressUnspent (BasicHotBitcoinAddressUnspent original)
+            : this (
+                original.HotBitcoinAddressUnspentId, original.TransactionHash, original.TransactionOutputIndex,
+                original.AmountSatoshis, original.ConfirmationCount)
+        {
+            // copy ctor
+        }
+
+        public int HotBitcoinAddressUnspentId { get; private set; }
+        public string TransactionHash { get; private set; }
+        public int TransactionOutputIndex { get; private set; }
+        public Int64 AmountSatoshis { get; private set; }
+        public int ConfirmationCount { get; protected set; }
+        
+        public int Identity { get { return HotBitcoinAddressUnspentId; }}
+    }
+}
