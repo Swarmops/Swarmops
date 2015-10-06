db04b2d2059d460f368905e0f97584b8889da64f
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2015-10-07 00:12:58 +0200

    Added Logic types for bitcoin hotwallet

---
+++ Logic/Financial/HotBitcoinAddress.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Swarmops.Basic.Types.Financial;
+
+namespace Swarmops.Logic.Financial
+{
+    public class HotBitcoinAddress: BasicHotBitcoinAddress
+    {
+        private HotBitcoinAddress(BasicHotBitcoinAddress basic): base (basic)
+        {
+            // private ctor
+        }
+
+        public static HotBitcoinAddress FromBasic (BasicHotBitcoinAddress basic)
+        {
+            return new HotBitcoinAddress (basic); // invoke private ctor
+        }
+
+
+    }
+}
---
+++ Logic/Financial/HotBitcoinAddressUnspent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Swarmops.Basic.Types.Financial;
+
+namespace Swarmops.Logic.Financial
+{
+    public class HotBitcoinAddressUnspent: BasicHotBitcoinAddressUnspent
+    {
+        private HotBitcoinAddressUnspent (BasicHotBitcoinAddressUnspent basic): base (basic)
+        {
+            // private ctor
+        }
+
+        public static HotBitcoinAddressUnspent FromBasic (BasicHotBitcoinAddressUnspent basic)
+        {
+            return new HotBitcoinAddressUnspent (basic);
+        }
+    }
+}
