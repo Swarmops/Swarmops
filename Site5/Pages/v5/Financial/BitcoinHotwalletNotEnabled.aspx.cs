f746ac618272d2fb0e1de3de9a6776ab32399c36
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2016-04-13 09:15:46 +0200

    Added a fail page

---
+++ Site5/Pages/v5/Financial/BitcoinHotwalletNotEnabled.aspx.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public partial class Pages_v5_Finance_BitcoinHotwallet : PageV5Base
+{
+    protected void Page_Load (object sender, EventArgs e)
+    {
+        PageTitle = "Hotwallet Not Enabled";
+        PageIcon = "iconshock-coins-cross";
+        InfoBoxLiteral = "Please contact an organization administrator for assistance.";
+    }
+}
