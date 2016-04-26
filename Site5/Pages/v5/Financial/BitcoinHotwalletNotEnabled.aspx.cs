c4477dcdb14d56b527e33a9167ca5d83453d80a5
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2016-04-26 07:56:23 +0200

    Dumbfix

--- Site5/Pages/v5/Financial/BitcoinHotwalletNotEnabled.aspx.cs
+++ Site5/Pages/v5/Financial/BitcoinHotwalletNotEnabled.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Swarmops.Logic.Security;
 
 public partial class Pages_v5_Finance_BitcoinHotwalletNotEnabled : PageV5Base
 {
@@ -7,5 +8,7 @@ public partial class Pages_v5_Finance_BitcoinHotwalletNotEnabled : PageV5Base
         PageTitle = "Hotwallet Not Enabled";
         PageIcon = "iconshock-coins-cross";
         InfoBoxLiteral = "Please contact an organization administrator for assistance.";
+
+        this.PageAccessRequired = new Access (AccessAspect.Null);
     }
 }
