3cf461e50c45cccb0c8df3c9bb8945f1633f8486
Falkvinge <rick.falkvinge@pirateacademy.eu>
2016-11-12 15:41:21 +0100

    More tweaks for VS2015

--- Logic/Communications/Payload/ParticipantMailPayload.cs
+++ Logic/Communications/Payload/ParticipantMailPayload.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient.Properties;
 //using Swarmops.Basic.Types.Common;
 using Swarmops.Logic.Security;
 using Swarmops.Logic.Swarm;
