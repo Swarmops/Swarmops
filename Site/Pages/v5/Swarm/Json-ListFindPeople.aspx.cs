b6bc3d7da4369fee83344ade9f1ec8b6184e8221
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2016-12-15 18:55:26 +0100

    One last fix for sprint O8

--- Site/Pages/v5/Swarm/Json-ListFindPeople.aspx.cs
+++ Site/Pages/v5/Swarm/Json-ListFindPeople.aspx.cs
@@ -49,7 +49,7 @@ namespace Swarmops.Frontend.Pages.Swarm
             {
                 string onePerson = '{' +
                                    String.Format (
-                                       "\"id\":\"{0}\",\"name\":\"<span class='spanUser{0}Name'>{1}</span>\",\"avatar16Url\":\"{2}\",\"geographyName\":\"{3}\",\"mail\":\"{4}\",\"phone\":\"{5}\"",
+                                       "\"id\":\"{0}\",\"name\":\"<span class='spanUser{0}Name'>{1}</span>\",\"avatar16Url\":\"{2}\",\"geographyName\":\"{3}\",\"mail\":\"<span class='spanUser{0}Mail'>{4}</span>\",\"phone\":\"<span class='spanUser{0}Phone'>{5}</span>\"",
                                        person.Identity,
                                        JsonSanitize (person.Canonical),
                                        person.GetSecureAvatarLink (16),
