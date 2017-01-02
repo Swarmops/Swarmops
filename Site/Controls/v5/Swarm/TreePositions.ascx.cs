6bb384e26927e1f337b65ff6a976ce434d724d1b
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2017-01-02 20:25:35 +0100

    Debugging on Sandbox

--- Site/Controls/v5/Swarm/TreePositions.ascx.cs
+++ Site/Controls/v5/Swarm/TreePositions.ascx.cs
@@ -37,14 +37,13 @@ namespace Swarmops.Frontend.Controls.Swarm
             this.LiteralButtonAssign.Text = Resources.Controls.Swarm.Positions_ButtonAssign;
 
             this.DropDuration.Items.Add (new ListItem(Resources.Global.Timespan_Selection_OneMonth, "1"));
+            throw new NotImplementedException();  // Debugging to find the Null Reference
             this.DropDuration.Items.Add(new ListItem(Resources.Global.Timespan_Selection_TwoMonths, "2"));
             this.DropDuration.Items.Add(new ListItem(Resources.Global.Timespan_Selection_ThreeMonths, "3"));
             this.DropDuration.Items.Add(new ListItem(Resources.Global.Timespan_Selection_SixMonths, "6"));
             this.DropDuration.Items.Add(new ListItem(Resources.Global.Timespan_Selection_OneYear, "12"));
             this.DropDuration.Items.Add(new ListItem(Resources.Global.Timespan_Selection_TwoYears, "24"));
             this.DropDuration.Items.Add(new ListItem(Resources.Global.Timespan_Selection_UntilTermination, "-1"));
-
-            throw new NotImplementedException();  // Debugging to find the Null Reference
 
             this.LiteralTerminateNo.Text = JavascriptEscape (Resources.Controls.Swarm.Positions_TerminateNo);
             this.LiteralTerminateYes.Text = JavascriptEscape (Resources.Controls.Swarm.Positions_TerminateYes);
