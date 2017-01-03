2f818dc72bb1421a317ac95c9fc00ef3dad3d21c
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2017-01-03 18:15:22 +0100

    Yeah I forgot a file. The build broke. My bad totally.

---
+++ Logic/Communications/IHasGeography.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Swarmops.Logic.Structure;
+
+namespace Swarmops.Logic.Communications
+{
+    public interface IHasGeography
+    {
+        Geography Geography { get; }
+    }
+}
