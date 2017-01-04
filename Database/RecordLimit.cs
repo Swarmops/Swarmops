3dd481a57de99942bbdb7675c13db40d935880cd
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2017-01-04 02:27:54 +0100

    Yeah I forgot a file again. My bad

---
+++ Database/RecordLimit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swarmops.Database
+{
+    public class RecordLimit
+    {
+        public RecordLimit() { }
+
+        public RecordLimit(int limit)
+        {
+            this.Limit = limit;
+        }
+
+        public int Limit { get; set; }
+    }
+}
