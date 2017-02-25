1b8a219410bb794fb780af5dafb20f57b312b367
Falkvinge <rick.falkvinge@pirateacademy.eu>
2017-02-25 05:09:39 +0100

    Cleaner login looking decent now

--- Site/Security/Login.aspx.designer.cs
+++ Site/Security/Login.aspx.designer.cs
@@ -112,6 +112,15 @@ namespace Swarmops.Pages.Security {
         protected global::System.Web.UI.WebControls.Image ImageBitIdQr;
         
         /// <summary>
+        /// LabelManualLoginHeader control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelManualLoginHeader;
+        
+        /// <summary>
         /// LiteralCredentialsUser control.
         /// </summary>
         /// <remarks>
@@ -137,6 +146,33 @@ namespace Swarmops.Pages.Security {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Literal LiteralCredentials2FA;
+        
+        /// <summary>
+        /// LabelForgotPassword control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelForgotPassword;
+        
+        /// <summary>
+        /// LabelManualLogin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelManualLogin;
+        
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
         
         /// <summary>
         /// PanelCheat control.
