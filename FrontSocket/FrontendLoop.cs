69bfdd208afe70b3ecf390d13460a6908b1d5685
Falkvinge <rick.falkvinge@pirateacademy.eu>
2017-03-23 01:35:34 +0100

    Forcing build

--- FrontSocket/FrontendLoop.cs
+++ FrontSocket/FrontendLoop.cs
@@ -293,7 +293,7 @@ namespace Swarmops.Frontend.Socket
 
             if (messageType == "InternalHeartbeat")
             {
-                // changing to something to force build
+                // changing to something else to force build again
             }
             else
             {
