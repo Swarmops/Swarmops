52d7c7819cffe8c87e817ba7b70909a7b1a6e615
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2017-07-26 08:50:39 +0200

    Work in Progress

---
+++ Site/Pages/v5/Financial/CreateOutboundInvoice.aspx
@@ -0,0 +1,190 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master-v5.master" AutoEventWireup="true" Inherits="Swarmops.Frontend.Pages.v5.Financial.CreateOutboundInvoice" CodeFile="CreateOutboundInvoice.aspx.cs" Codebehind="CreateOutboundInvoice.aspx.cs" %>
+<%@ Register src="~/Controls/v5/Base/FileUpload.ascx" tagname="FileUpload" tagprefix="Swarmops5" %>
+<%@ Register TagPrefix="Swarmops5" TagName="ComboBudgets" Src="~/Controls/v5/Financial/ComboBudgets.ascx" %>
+<%@ Register TagPrefix="Swarmops5" TagName="CurrencyAmount" Src="~/Controls/v5/Financial/CurrencyTextBox.ascx" %>
+
+<asp:Content ID="Content4" ContentPlaceHolderID="PlaceHolderHead" Runat="Server">
+    <!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
+    <script src="/Scripts/jquery.fileupload/jquery.iframe-transport.js" type="text/javascript" language="javascript"></script>
+    <!-- The basic File Upload plugin -->
+    <script src="/Scripts/jquery.fileupload/jquery.fileupload.js" type="text/javascript" language="javascript"></script>
+
+    <script type="text/javascript">
+
+        $(document).ready(function () {
+
+            if (vatEnabled) { // if VAT is enabled, show those controls
+                $('.vatEnabled').show();
+            }
+
+            <asp:Repeater ID="RepeaterTagDropScript" runat="server"><ItemTemplate>
+            $('#DropTags<%# Eval("TagSetId") %>').combotree({
+                animate: true,
+                height: 30
+            });
+
+            
+            $('#SpanDropTags<%# Eval("TagSetId") %> span.combo input.combo-text').click(function () {
+                $('#SpanDropTags<%# Eval("TagSetId") %> span.combo span span.combo-arrow').click();
+            });
+            </ItemTemplate></asp:Repeater>
+
+
+            if (successMessage.length > 0) {
+                alertify.success(successMessage);  // displayed on successful creation of invoice
+            }
+        });
+
+
+        function validateFields() {
+            var isValid = true;
+
+            isValid = validateTextField('#<%=this.TextAccount.ClientID %>', SwarmopsJS.unescape('<%= this.Localized_ValidationError_Account %>')) && isValid;
+
+            if ($('#<%=this.ComboBudgets.ClientID %>_DropBudgets').combotree('tree').tree('getSelected') == null) {
+                isValid = false;
+                $('#<%=this.ComboBudgets.ClientID %>_SpanBudgets').addClass("entryError");
+                alertify.error(SwarmopsJS.unescape('<%= this.Localized_ValidationError_Budget %>'));
+            }
+
+            <asp:Repeater ID="RepeaterErrorCheckTags" runat="server"><ItemTemplate>
+                if ($('#DropTags<%# Eval("TagSetId") %>').combotree('tree').tree('getSelected') == null) {
+                    isValid = false;
+                    $('#SpanDropTags<%# Eval("TagSetId") %>').addClass("entryError");
+                    alertify.error(SwarmopsJS.unescape('<%=this.Localized_ValidationError_MissingTag %>'));
+                }
+            </ItemTemplate></asp:Repeater>
+
+            isValid = validateTextField('#<%=this.TextPurpose.ClientID %>', SwarmopsJS.unescape('<%= this.Localized_ValidationError_Purpose %>')) && isValid;
+
+            var jsonData = {};
+            jsonData.amount = <%=this.CurrencyAmount.ClientID %>_val();
+
+            $.ajax({
+                type: "POST",
+                url: "/Automation/FieldValidation.aspx/IsAmountValid",
+                data: $.toJSON(jsonData),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                async: false, // blocks until function returns - race conditions otherwise
+                success: function(msg) {
+                    if (msg.d != true) {
+                        isValid = false;
+                        $('#<%=CurrencyAmount.ClientID %>_TextInput').addClass("entryError");
+                        alertify.error(SwarmopsJS.unescape('<%= this.Localized_ValidationError_Amount %>'));
+                        <%=CurrencyAmount.ClientID %>_focus();
+                    }
+                }
+            });
+
+            if (vatEnabled) {
+                $.ajax({
+                    type: "POST",
+                    url: "/Automation/FieldValidation.aspx/IsAmountValid",
+                    data: $.toJSON(jsonData),
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                    async: false, // blocks until function returns - race conditions otherwise
+                    success: function(msg) {
+                        if (msg.d != true) {
+                            isValid = false;
+                            $('#<%=CurrencyAmount.ClientID %>_TextInput').addClass("entryError");
+                            alertify.error(SwarmopsJS.unescape('<%= this.Localized_ValidationError_Amount %>'));
+                            <%=CurrencyAmount.ClientID %>_focus();
+                        }
+                    }
+                });
+            }
+
+            $.ajax({
+                type: "POST",
+                url: "/Automation/FieldValidation.aspx/AreDocumentsUploaded",
+                data: "{'guidString': '<%=this.FileUpload.GuidString %>'}",
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                async: false,  // blocks until function returns - race conditions otherwise
+                success: function (msg) {
+                    if (msg.d != true) {
+                        isValid = false;
+                        $('#TextAmount').addClass("entryError");
+                        alertify.error(SwarmopsJS.unescape('<%= this.Localized_ValidationError_Documents %>'));
+                    }
+                }
+            });
+
+            return isValid;
+        }
+        
+        function validateTextField (fieldId, message) {
+            if ($(fieldId).val().length == 0) {
+                alertify.error(message);
+                $(fieldId).addClass("entryError");
+                $(fieldId).focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        var vatEnabled = <%= this.CurrentOrganization.FinancialAccounts.AssetsVatInbound == null? "false": "true" %>;
+        var successMessage = SwarmopsJS.unescape('<%= this.Localized_ForGreatJustice %>');
+
+    </script>
+    
+    <style type="text/css">
+        
+        .vatEnabled
+        { display: none;}
+
+    </style>
+
+</asp:Content>
+
+
+<asp:Content ID="Content5" ContentPlaceHolderID="PlaceHolderMain" Runat="Server">
+    <h2><asp:Label ID="BoxTitle" runat="server"/></h2>
+    <asp:HiddenField ID="HiddenTagSetIdentifiers" runat="server"/>
+    <div class="entryFields">
+        <div class="stacked-input-control"><asp:TextBox runat="server" ID="TextSupplier" /></div>
+        <Swarmops5:CurrencyAmount runat="server" ID="CurrencyAmount" />
+        <span class="vatEnabled"><Swarmops5:CurrencyAmount runat="server" ID="CurrencyVat" /></span>
+        <div class="stacked-input-control"><asp:TextBox runat="server" ID="TextPurpose" /></div>
+        <div class="stacked-input-control"><asp:TextBox runat="server" ID="TextDueDate" /></div>
+        <Swarmops5:ComboBudgets ID="ComboBudgets" runat="server" />
+        <asp:Repeater ID="RepeaterTagDrop" runat="server"><ItemTemplate><span id="SpanDropTags<%# Eval("TagSetId") %>"><select class="easyui-combotree" url="/Automation/Json-TransactionTagsTree.aspx?TagSetId=<%# Eval("TagSetId") %>" name="DropTags<%# Eval("TagSetId") %>" id="DropTags<%# Eval("TagSetId") %>" animate="true" style="width:300px"></select></span>&nbsp;<br/></ItemTemplate></asp:Repeater>
+
+       <div class="stacked-input-control"></div> <!-- placeholder for label-side H2 -->
+        
+        <!-- file upload begins here -->
+        
+        <Swarmops5:FileUpload ID="FileUpload" runat="server" Filter="ImagesOrPdf"/>
+
+        <!-- file upload ends -->
+
+        <div class="stacked-input-control"></div> <!-- placeholder for label-side H2 -->
+        <div class="stacked-input-control"><asp:TextBox runat="server" ID="TextAccount" /></div>
+        <div class="stacked-input-control"><asp:TextBox runat="server" ID="TextReference" /></div>
+        <asp:Button ID="ButtonCreate" runat="server" CssClass="buttonAccentColor NoInputFocus" OnClientClick="return validateFields();" OnClick="ButtonCreate_Click" Text="Create"/>
+    </div>
+    <div class="entryLabels">
+        <asp:Label runat="server" ID="LabelSupplier" /><br/>
+        <asp:Label runat="server" ID="LabelAmount" /><br/>
+        <span class="vatEnabled"><asp:Label runat="server" ID="LabelVat" /><br/></span>
+        <asp:Label runat="server" ID="LabelPurpose" /><br/>
+        <asp:Label runat="server" ID="LabelDueDate" /><br/>
+        <asp:Label runat="server" ID="LabelBudget" /><br/>
+        <asp:Repeater ID="RepeaterTagLabels" runat="server"><ItemTemplate><%# Eval("TagSetLocalizedName") %><br/></ItemTemplate></asp:Repeater>
+        <h2><asp:Label runat="server" ID="LabelHeaderImageFiles" /></h2>
+        <asp:Label runat="server" ID="LabelImageFiles" /><br/>
+        <h2><asp:Label runat="server" ID="LabelHeaderBankDetails" /></h2>
+        <asp:Label runat="server" ID="LabelAccount" /><br/>
+        <asp:Label runat="server" ID="LabelReference" /><br/>
+    </div>
+    <div style="clear:both"></div>
+</asp:Content>
+
+
+
+<asp:Content ID="Content6" ContentPlaceHolderID="PlaceHolderSide" Runat="Server">
+</asp:Content>
+
---
+++ Site/Pages/v5/Financial/CreateOutboundInvoice.aspx.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Web;
+using Resources;
+using Swarmops.Logic.Financial;
+using Swarmops.Logic.Security;
+using Swarmops.Logic.Support;
+
+namespace Swarmops.Frontend.Pages.v5.Financial
+{
+    public partial class CreateOutboundInvoice : PageV5Base
+    {
+        private int[] _tagSetIds;
+
+        protected void Page_Init (object sender, EventArgs e)
+        {
+            string tagSetIdsString = Request["ctl00$PlaceHolderMain$HiddenTagSetIdentifiers"];
+
+            // Find our tag ids, either from previously hidden var or load from org
+
+            if (String.IsNullOrEmpty (tagSetIdsString))
+            {
+                this._tagSetIds = FinancialTransactionTagSets.ForOrganization (CurrentOrganization).Identities;
+            }
+            else
+            {
+                string[] tagSetIdStrings = tagSetIdsString.Split (',');
+                this._tagSetIds = new int[tagSetIdStrings.Length];
+
+                for (int index = 0; index < tagSetIdStrings.Length; index++)
+                {
+                    this._tagSetIds[index] = Int32.Parse (tagSetIdStrings[index]);
+                }
+            }
+
+            // Construct data source
+
+            List<TagSetDataSourceItem> dataSourceVisibleTags = new List<TagSetDataSourceItem>();
+            List<TagSetDataSourceItem> dataSourceForcedTags = new List<TagSetDataSourceItem>();
+
+            foreach (int tagSetId in this._tagSetIds)
+            {
+                TagSetDataSourceItem item = new TagSetDataSourceItem
+                {
+                    TagSetId = tagSetId,
+                    TagSetLocalizedName =
+                        FinancialTransactionTagSetType.GetLocalizedName (
+                            FinancialTransactionTagSet.FromIdentity (tagSetId).
+                                FinancialTransactionTagSetTypeId)
+                };
+                dataSourceVisibleTags.Add (item);
+
+                if (!FinancialTransactionTagSet.FromIdentity (tagSetId).AllowUntagged)
+                {
+                    dataSourceForcedTags.Add (item);
+                }
+            }
+
+            // Bind data
+
+            this.RepeaterTagLabels.DataSource = dataSourceVisibleTags;
+            this.RepeaterTagDrop.DataSource = dataSourceVisibleTags;
+            this.RepeaterTagDropScript.DataSource = dataSourceVisibleTags;
+            this.RepeaterErrorCheckTags.DataSource = dataSourceForcedTags;
+
+            this.RepeaterTagLabels.DataBind();
+            this.RepeaterTagDrop.DataBind();
+            this.RepeaterTagDropScript.DataBind();
+            this.RepeaterErrorCheckTags.DataBind();
+
+            // Write set list back to hidden variable
+
+            List<string> tagSetIdStringList = new List<string>();
+
+            foreach (int tagSetId in this._tagSetIds)
+            {
+                tagSetIdStringList.Add (tagSetId.ToString (CultureInfo.InvariantCulture));
+            }
+
+            this.HiddenTagSetIdentifiers.Value = String.Join (",", tagSetIdStringList.ToArray());
+        }
+
+        protected void Page_Load (object sender, EventArgs e)
+        {
+            if (!CurrentOrganization.IsEconomyEnabled)
+            {
+                Response.Redirect ("/Pages/v5/Financial/EconomyNotEnabled.aspx", true);
+                return;
+            }
+
+            this.BoxTitle.Text = PageTitle = Resources.Pages.Financial.CreateInboundInvoice_PageTitle;
+            PageIcon = "iconshock-invoice";
+            InfoBoxLiteral = Resources.Pages.Financial.CreateInboundInvoice_Info;
+
+            PageAccessRequired = new Access (CurrentOrganization, AccessAspect.Secretarial, AccessType.Write);
+
+            if (!Page.IsPostBack)
+            {
+                // Prime bank details
+
+                this.CurrencyAmount.Cents = 0;
+                this.CurrencyVat.Cents = 0;
+                this.TextSupplier.Focus();
+                this.TextDueDate.Text = DateTime.Today.AddDays (30).ToShortDateString(); // Use current culture
+
+                Localize();
+            }
+        }
+
+
+        private void Localize()
+        {
+            this.LabelSupplier.Text = Global.Financial_Supplier;
+            this.LabelDueDate.Text = Global.Financial_DueDate;
+            this.LabelAmount.Text = string.Format (Resources.Pages.Financial.CreateInboundInvoice_Amount,
+                CurrentOrganization.Currency.Code);
+            this.LabelVat.Text = string.Format(Resources.Pages.Financial.CreateInboundInvoice_AmountVat,
+                CurrentOrganization.Currency.Code);
+            this.LabelPurpose.Text = Resources.Pages.Financial.CreateInboundInvoice_Description;
+            this.LabelBudget.Text = Global.Financial_Budget;
+            this.LabelHeaderBankDetails.Text = Resources.Pages.Financial.CreateInboundInvoice_HeaderPaymentDetails;
+            this.LabelHeaderImageFiles.Text = Resources.Pages.Financial.CreateInboundInvoice_HeaderInvoiceImage;
+            this.LabelImageFiles.Text = Resources.Pages.Financial.CreateInboundInvoice_UploadInvoiceImage;
+            this.LabelReference.Text = Resources.Pages.Financial.CreateInboundInvoice_Reference;
+            this.LabelAccount.Text = Resources.Pages.Financial.CreateInboundInvoice_SupplierAccount;
+
+            this.ButtonCreate.Text = Resources.Pages.Financial.CreateInboundInvoice_ButtonCreate;
+        }
+
+
+        protected void ButtonCreate_Click (object sender, EventArgs e) // TODO
+        {
+            // The data has been validated client-side already. We'll throw unfriendly exceptions if invalid data is passed here.
+            // People who choose to disable JavaScript and then submit bad input almost deserve to be hurt.
+
+            Int64 amountCents = this.CurrencyAmount.Cents;
+            Int64 amountVatCents = this.CurrencyVat.Cents;
+
+            string description = this.TextPurpose.Text;
+
+            DateTime dueDate = DateTime.Parse (this.TextDueDate.Text);
+
+
+            FinancialAccount budget = FinancialAccount.FromIdentity (Int32.Parse (Request.Form["DropBudgets"]));
+
+            // sanity check
+
+            if (budget.Organization.Identity != CurrentOrganization.Identity)
+            {
+                throw new InvalidOperationException ("Budget-organization mismatch; won't file expense claim");
+            }
+
+
+            // Get documents; check that documents have been uploaded
+
+            Documents documents = Documents.RecentFromDescription (this.FileUpload.GuidString);
+
+            if (documents.Count == 0)
+            {
+                throw new InvalidOperationException ("No documents uploaded");
+            }
+
+            InboundInvoice invoice = InboundInvoice.Create (CurrentOrganization, dueDate, amountCents, amountVatCents, budget,
+                this.TextSupplier.Text, this.TextPurpose.Text, this.TextAccount.Text, string.Empty,
+                this.TextReference.Text, CurrentUser);
+
+            foreach (int tagSetId in this._tagSetIds)
+            {
+                string selectedTagString =
+                    Request.Form["DropTags" + tagSetId.ToString (CultureInfo.InvariantCulture)];
+
+                if (!String.IsNullOrEmpty (selectedTagString))
+                {
+                    int selectedTagType = Int32.Parse (selectedTagString);
+                    if (selectedTagType != 0)
+                    {
+                        invoice.FinancialTransaction.CreateTag (
+                            FinancialTransactionTagType.FromIdentity (selectedTagType), CurrentUser);
+                    }
+                }
+            }
+
+            documents.SetForeignObjectForAll (invoice);
+
+            // Display success message
+
+            this._invoiceId = invoice.Identity; // a property returns the localized string
+
+            // Reset all fields for next invoice
+
+            this.FileUpload.Reset();
+            this.TextSupplier.Text = String.Empty;
+            this.TextAccount.Text = String.Empty;
+            this.TextPurpose.Text = String.Empty;
+            this.TextReference.Text = String.Empty;
+            this.CurrencyAmount.Cents = 0;
+            this.CurrencyVat.Cents = 0;
+            this.TextDueDate.Text = DateTime.Today.AddDays (30).ToShortDateString(); // Use current culture
+
+            // the easyUI combo fields should reset automatically on form submission unless we explicitly reconstruct
+
+            this.TextSupplier.Focus();
+        }
+
+
+        private int _invoiceId = 0;
+
+
+        protected class TagSetDataSourceItem
+        {
+            public int TagSetId { get; set; }
+            public string TagSetLocalizedName { get; set; }
+        }
+
+        // ReSharper disable InconsistentNaming
+        public string Localized_ForGreatJustice
+        {
+            get
+            {
+                if (this._invoiceId == 0)
+                {
+                    return string.Empty;
+                }
+
+                return
+                    JavascriptEscape (String.Format (Resources.Pages.Financial.CreateInboundInvoice_SuccessMessage,
+                        this._invoiceId));
+            }
+        }
+
+        public string Localized_ValidationError_MissingTag
+        {
+            get { return JavascriptEscape(Resources.Pages.Financial.CreateInboundInvoice_ValidationError_MissingTag); }
+        }
+
+        public string Localized_ValidationError_Account
+        {
+            get { return JavascriptEscape(Resources.Pages.Financial.CreateInboundInvoice_ValidationError_Account); }
+        }
+
+        public string Localized_ValidationError_Purpose
+        {
+            get { return JavascriptEscape(Resources.Pages.Financial.CreateInboundInvoice_ValidationError_Purpose); }
+        }
+
+        public string Localized_ValidationError_Budget
+        {
+            get { return JavascriptEscape(Resources.Pages.Financial.CreateInboundInvoice_ValidationError_Budget); }
+        }
+
+        public string Localized_ValidationError_Amount
+        {
+            get { return JavascriptEscape(Resources.Pages.Financial.CreateInboundInvoice_ValidationError_Amount); }
+        }
+
+        public string Localized_ValidationError_Documents
+        {
+            get { return JavascriptEscape(Resources.Pages.Financial.CreateInboundInvoice_ValidationError_Documents); }
+        }
+
+    }
+}
