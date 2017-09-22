72c49e086a77d204f0dd0c25037f0a9c54c8abf5
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2017-09-22 20:49:59 +0200

    Cosmetics

--- Site/Pages/v5/Financial/Json-ListInvoicesInbound.aspx.cs
+++ Site/Pages/v5/Financial/Json-ListInvoicesInbound.aspx.cs
@@ -36,7 +36,7 @@ public partial class Pages_v5_Finance_Json_ListInvoicesInbound : DataV5Base
         StringBuilder result = new StringBuilder (16384);
 
         string hasDoxString =
-            "<img src='/Images/Icons/iconshock-search-256px.png' onmouseover=\\\"this.src='/Images/Icons/iconshock-search-hot-256px.png';\\\" onmouseout=\\\"this.src='/Images/Icons/iconshock-search-256px.png';\\\" baseid='{6}' class='LocalViewDox' style='cursor:pointer' />";
+            "<img src='/Images/Icons/iconshock-search-256px.png' onmouseover=\\\"this.src='/Images/Icons/iconshock-search-hot-256px.png';\\\" onmouseout=\\\"this.src='/Images/Icons/iconshock-search-256px.png';\\\" baseid='{6}' class='LocalViewDox' style='cursor:pointer' height='20' width='20' />";
 
         result.Append ("{\"rows\":[");
 
