445f1cf07b6d5ca7fa9ee9e67a41b725e15f935d
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2017-10-05 17:02:23 +0200

    Adding VAT report classes (basic)

---
+++ Basic/Types/Financial/BasicVatReport.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Swarmops.Common.Interfaces;
+
+namespace Swarmops.Basic.Types.Financial
+{
+    public class BasicVatReport: IHasIdentity
+    {
+        // Basic ctor
+
+
+
+        // Copy ctor
+
+
+
+        // Properties
+
+        public int VatReportId { get; private set; }
+        public int OrganizationId { get; private set; }
+        public string Guid { get; private set; }
+        public int YearMonth { get; private set; }
+        public int MonthCount { get; private set; }
+        public Int64 TurnoverCents { get; protected set; }
+        public Int64 VatOutboundCents { get; protected set; }
+        public Int64 VatInboundCents { get; protected set; }
+        public bool Open { get; protected set; }
+        public bool UnderConstruction { get; protected set; }
+
+        // Interfaces
+
+        public int Identity
+        {
+            get { return this.VatReportId; }
+        }
+    }
+}
---
+++ Basic/Types/Financial/BasicVatReportItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Swarmops.Common.Enums;
+using Swarmops.Common.Interfaces;
+
+namespace Swarmops.Basic.Types.Financial
+{
+    public class BasicVatReportItem: IHasIdentity
+    {
+        // Normal ctor
+
+        // Copy ctor
+
+        // Properties
+
+        public int VatReportItemId { get; private set; }
+
+        public int ForeignId { get; private set; }
+        public int FinancialTransactionId { get; private set; }
+        public FinancialDependencyType DependencyType { get; private set; }
+
+        // Interfaces
+
+        public int Identity { get { return this.VatReportItemId; } }
+    }
+}
