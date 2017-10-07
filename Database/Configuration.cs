b0b551e3d47195826c67f2c7a8d61f49c2db0989
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2017-10-07 12:35:13 +0200

    Mark Bitcoin as cryptocurrency

--- Database/Configuration.cs
+++ Database/Configuration.cs
@@ -14,7 +14,7 @@ namespace Swarmops.Database
         /// </summary>
         public static int DbVersionExpected
         {
-            get { return 43; }
+            get { return 44; }
         }
 
 
