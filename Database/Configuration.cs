35cf1438cd983947e6d23d5fee53e9b6ec4f44f9
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2017-10-09 09:04:19 +0200

    Dbupgrade 0048 - procedure fix

--- Database/Configuration.cs
+++ Database/Configuration.cs
@@ -14,7 +14,7 @@ namespace Swarmops.Database
         /// </summary>
         public static int DbVersionExpected
         {
-            get { return 47; }
+            get { return 48; }
         }
 
 
---
+++ Database/Schemata/upgrade-0048.sql
@@ -0,0 +1,36 @@
+﻿DROP PROCEDURE IF EXISTS `CreateVatReportItem`
+
+#
+
+CREATE PROCEDURE `CreateVatReportItem`(
+  vatReportId INT,
+  financialTransactionId INT,
+  foreignId INT,
+  financialDependencyType VARCHAR(64),
+  turnoverCents BIGINT,
+  vatInboundCents BIGINT,
+  vatOutboundCents BIGINT
+)
+BEGIN
+
+  DECLARE financialDependencyTypeId INTEGER;
+
+  SELECT 0 INTO financialDependencyTypeId;
+
+  IF ((SELECT COUNT(*) FROM FinancialDependencyTypes WHERE FinancialDependencyTypes.Name=financialDependencyType) = 0)
+  THEN
+    INSERT INTO FinancialDependencyTypes (Name)
+      VALUES (financialDependencyType);
+
+    SELECT LAST_INSERT_ID() INTO financialDependencyTypeId;
+
+  ELSE
+
+    SELECT FinancialDependencyTypes.FinancialDependencyTypeId INTO FinancialDependencyTypeId FROM FinancialDependencyTypes
+        WHERE FinancialDependencyTypes.Name=financialDependencyType;
+
+  END IF;
+
+  INSERT INTO VatReportItems (VatReportId, FinancialTransactionId, ForeignId, FinancialDependencyTypeId, TurnoverCents, VatInboundCents, VatOutboundCents)
+    VALUES (vatReportId, financialTransactionId, foreignId, financialDependencyTypeId, turnoverCents, vatInboundCents, vatOutboundCents);
+END
