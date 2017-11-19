70deaad23e4791cf22b4efb9ddc89514b1dccd2f
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2017-11-19 21:38:41 +0100

    Bugfix to Db0052

--- Database/Configuration.cs
+++ Database/Configuration.cs
@@ -14,7 +14,7 @@ namespace Swarmops.Database
         /// </summary>
         public static int DbVersionExpected
         {
-            get { return 52; }
+            get { return 53; }
         }
 
 
---
+++ Database/Schemata/upgrade-0053.sql
@@ -0,0 +1,34 @@
+﻿DROP PROCEDURE IF EXISTS `CreateHotBitcoinAddressConditional`
+
+#
+
+
+CREATE PROCEDURE `CreateHotBitcoinAddressConditional`(
+  IN organizationId INT,
+  IN bitcoinChainId INT,
+  IN derivationPath VARCHAR(256),
+  IN addressString VARCHAR(48),
+  IN addressStringFallback VARCHAR(48)
+)
+BEGIN
+
+  IF ((SELECT COUNT(*) FROM HotBitcoinAddresses WHERE HotBitcoinAddresses.AddressString=addressString AND HotBitcoinAddresses.BitcoinChainId=bitcoinChainId) = 0)
+  THEN
+
+    INSERT INTO HotBitcoinAddresses
+      (OrganizationId,BitcoinChainId,DerivationPath,AddressString,AddressStringFallback,BalanceSatoshis,ThroughputSatoshis)
+    VALUES 
+      (organizationId,bitcoinChainId,derivationPath,addressString,addressStringFallback,0,0);
+
+    SELECT LAST_INSERT_ID() AS Identity;
+
+  ELSE
+
+    SELECT HotBitcoinAddresses.HotBitcoinAddressId AS Identity
+      FROM HotBitcoinAddresses
+      WHERE HotBitcoinAddresses.AddressString=addressString AND HotBitcoinAddresses.BitcoinChainId=bitcoinChainId;
+
+  END IF;
+
+END
+
