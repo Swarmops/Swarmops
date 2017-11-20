44dd7a1aa6490dafd798b243f08988aa0e3ac27c
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2017-11-20 12:26:59 +0100

    Db0054 - fix to Create-UTXO procedure with Cash

--- Database/Configuration.cs
+++ Database/Configuration.cs
@@ -14,7 +14,7 @@ namespace Swarmops.Database
         /// </summary>
         public static int DbVersionExpected
         {
-            get { return 53; }
+            get { return 54; }
         }
 
 
---
+++ Database/Schemata/upgrade-0054.sql
@@ -0,0 +1,58 @@
+﻿DROP PROCEDURE IF EXISTS `CreateHotBitcoinAddressUnspentConditional`
+
+
+#
+
+
+CREATE PROCEDURE `CreateHotBitcoinAddressUnspentConditional`(
+  IN hotBitcoinAddressId INT,
+  IN amountSatoshis BIGINT,
+  IN transactionHash VARCHAR(128),
+  IN transactionOutputIndex INT,
+  IN confirmationCount INT
+)
+BEGIN
+
+  DECLARE unspentId INT;
+
+  IF ((SELECT COUNT(*) FROM HotBitcoinAddressUnspents 
+    WHERE HotBitcoinAddressUnspents.TransactionHash=transactionHash
+      AND HotBitcoinAddressUnspents.TransactionOutputIndex=transactionOutputIndex
+      AND HotBitcoinAddressUnspents.HotBitcoinAddressId=hotBitcoinAddressId) = 0)
+  THEN
+
+    INSERT INTO HotBitcoinAddressUnspents
+      (HotBitcoinAddressId,AmountSatoshis,TransactionHash,TransactionOutputIndex,ConfirmationCount)
+    VALUES 
+      (hotBitcoinAddressId,amountSatoshis,transactionHash,transactionOutputIndex,confirmationCount);
+
+    SELECT LAST_INSERT_ID() INTO unspentId;
+
+  ELSE
+
+    SELECT HotBitcoinAddressUnspents.HotBitcoinAddressUnspentId INTO unspentId
+      FROM HotBitcoinAddressUnspents
+      WHERE HotBitcoinAddressUnspents.TransactionHash=transactionHash
+      AND HotBitcoinAddressUnspents.TransactionOutputIndex=transactionOutputIndex
+      AND HotBitcoinAddressId=hotBitcoinAddressId;
+
+    UPDATE HotBitcoinAddressUnspents
+      SET HotBitcoinAddressUnspents.ConfirmationCount=confirmationCount
+      WHERE HotBitcoinAddressUnspents.HotBitcoinAddressUnspentId=unspentId;
+
+  END IF;
+
+  UPDATE HotBitcoinAddresses
+    INNER JOIN
+    (
+      SELECT HotBitcoinAddressId, SUM(AmountSatoshis) AS UnspentAmountSum
+      FROM HotBitcoinAddressUnspents
+      GROUP BY HotBitcoinAddressId
+    ) innerSelection ON HotBitcoinAddresses.HotBitcoinAddressId =
+        innerSelection.HotBitcoinAddressId
+    SET HotBitcoinAddresses.BalanceSatoshis = innerSelection.UnspentAmountSum
+    WHERE HotBitcoinAddresses.HotBitcoinAddressId=hotBitcoinAddressId;
+
+  SELECT unspentId AS Identity;
+
+END
