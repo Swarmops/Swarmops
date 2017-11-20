1331b4c32c4d82a3f7febce40cd1fadd7286fa28
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2017-11-20 13:09:21 +0100

    Bugfix for index collision introduced with Db0054

--- Database/Configuration.cs
+++ Database/Configuration.cs
@@ -14,7 +14,7 @@ namespace Swarmops.Database
         /// </summary>
         public static int DbVersionExpected
         {
-            get { return 54; }
+            get { return 55; }
         }
 
 
---
+++ Database/Schemata/upgrade-0055.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE `HotBitcoinAddressUnspents` 
+DROP INDEX `Ix_HashIndex` ,
+ADD UNIQUE INDEX `Ix_HashIndex` (`TransactionHash` ASC, `TransactionOutputIndex` ASC, `HotBitcoinAddressId` ASC)
