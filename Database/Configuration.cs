920560d1f3fceb93eae7951b00cd90b1c0507401
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2017-11-21 08:31:02 +0100

    Db0058: Bugfix for other half of 0056

--- Database/Configuration.cs
+++ Database/Configuration.cs
@@ -14,7 +14,7 @@ namespace Swarmops.Database
         /// </summary>
         public static int DbVersionExpected
         {
-            get { return 57; }
+            get { return 58; }
         }
 
 
---
+++ Database/Schemata/upgrade-0058.sql
@@ -0,0 +1,47 @@
+﻿DROP PROCEDURE IF EXISTS `UpdateHotBitcoinAddressUnspentTotals`
+
+#
+
+DROP PROCEDURE IF EXISTS `UpdateHotBitcoinAddressUnspentTotal`
+
+
+#
+
+
+CREATE PROCEDURE `UpdateHotBitcoinAddressUnspentTotal` (
+  IN hotBitcoinAddressId INTEGER
+)
+BEGIN
+
+  UPDATE HotBitcoinAddresses
+    INNER JOIN
+    (
+      SELECT HotBitcoinAddressId, SUM(AmountSatoshis) AS UnspentAmountSum
+      FROM HotBitcoinAddressUnspents
+      GROUP BY HotBitcoinAddressId
+    ) innerSelection ON HotBitcoinAddresses.HotBitcoinAddressId = innerSelection.HotBitcoinAddressId
+
+    SET HotBitcoinAddresses.BalanceSatoshis = innerSelection.UnspentAmountSum
+    WHERE HotBitcoinAddresses.HotBitcoinAddressId=hotBitcoinAddressId;
+
+END
+
+
+#
+
+
+CREATE PROCEDURE `UpdateHotBitcoinAddressUnspentTotals` ()
+BEGIN
+
+  UPDATE HotBitcoinAddresses
+    INNER JOIN
+    (
+      SELECT HotBitcoinAddressId, SUM(AmountSatoshis) AS UnspentAmountSum
+      FROM HotBitcoinAddressUnspents
+      GROUP BY HotBitcoinAddressId
+    ) innerSelection ON HotBitcoinAddresses.HotBitcoinAddressId = innerSelection.HotBitcoinAddressId
+
+    SET HotBitcoinAddresses.BalanceSatoshis = innerSelection.UnspentAmountSum;
+
+END
+
