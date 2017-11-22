ad47ce94ad89b02f41e4aa368fa112ebd62cfd59
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2017-11-22 22:56:59 +0100

    Db0060: Fix for a dumb problem

---
+++ Database/Schemata/upgrade-0060.sql
@@ -0,0 +1,8 @@
+﻿ALTER TABLE `BackendServiceClasses` 
+CHANGE COLUMN `BackendServiceClassId` `BackendServiceClassId` INT(11) NOT NULL AUTO_INCREMENT
+
+#
+
+ALTER TABLE `BackendServiceOrders` 
+CHANGE COLUMN `BackendServiceOrderId` `BackendServiceOrderId` INT(11) NOT NULL AUTO_INCREMENT
+
