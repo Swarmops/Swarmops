9fb41501af99f548706ce4db750a047bec8886f5
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2017-12-24 21:38:02 +0100

    Another cosmetic fix

--- Site/Pages/v5/Financial/Json-ValidatableReceipts.aspx.cs
+++ Site/Pages/v5/Financial/Json-ValidatableReceipts.aspx.cs
@@ -29,7 +29,7 @@ public partial class Pages_v5_Finance_Json_ValidatableReceipts : DataV5Base
         StringBuilder result = new StringBuilder (16384);
 
         string hasDoxString =
-            "<img src='/Images/Icons/iconshock-search-256px.png' onmouseover=\\\"this.src='/Images/Icons/iconshock-search-hot-256px.png';\\\" onmouseout=\\\"this.src='/Images/Icons/iconshock-search-256px.png';\\\" baseid=\\\"E{5}\\\" class=\\\"LocalViewDox\\\" height=\\\"18\\\" width=\\\"24\\\" style=\\\"cursor:pointer\\\" />";
+            "<img src='/Images/Icons/iconshock-search-256px.png' onmouseover=\\\"this.src='/Images/Icons/iconshock-search-hot-256px.png';\\\" onmouseout=\\\"this.src='/Images/Icons/iconshock-search-256px.png';\\\" baseid=\\\"E{5}\\\" class=\\\"LocalViewDox\\\" height=\\\"20\\\" width=\\\"20\\\" style=\\\"cursor:pointer\\\" />";
 
         result.Append ("{\"rows\":[");
         FinancialTransactionTagSets tagSets = FinancialTransactionTagSets.ForOrganization (CurrentOrganization);
@@ -55,11 +55,11 @@ public partial class Pages_v5_Finance_Json_ValidatableReceipts : DataV5Base
             result.AppendFormat (
                 "\"description\":\"{2}\",\"budgetName\":\"{3}\",{6}\"amountRequested\":\"{4:N2}\",\"itemId\":\"E{5}\"," +
                 "\"dox\":\"" + (claim.Documents.Count > 0 ? hasDoxString : "&nbsp;") + "\"," +
-                "\"actions\":\"<span style=\\\"position:relative;top:3px\\\">" +
+                "\"actions\":\"" +
                 "<img id=\\\"IconApprovalE{5}\\\" class=\\\"LocalIconApproval\\\" baseid=\\\"E{5}\\\" height=\\\"18\\\" width=\\\"24\\\" />" +
                 "<img id=\\\"IconApprovedE{5}\\\" class=\\\"LocalIconApproved\\\" baseid=\\\"E{5}\\\" height=\\\"18\\\" width=\\\"24\\\" />" +
                 "<img id=\\\"IconDenialE{5}\\\" class=\\\"LocalIconDenial\\\" baseid=\\\"E{5}\\\" height=\\\"18\\\" width=\\\"24\\\" />" +
-                "<img id=\\\"IconDeniedE{5}\\\" class=\\\"LocalIconDenied\\\" baseid=\\\"E{5}\\\" height=\\\"18\\\" width=\\\"24\\\" /></span>\"",
+                "<img id=\\\"IconDeniedE{5}\\\" class=\\\"LocalIconDenied\\\" baseid=\\\"E{5}\\\" height=\\\"18\\\" width=\\\"24\\\" />\"",
                 "olditem", JsonSanitize (claim.ClaimerCanonical), JsonSanitize (claim.Description),
                 JsonSanitize (claim.Budget.Name),
                 claim.AmountCents/100.0, claim.Identity, extraTags);
