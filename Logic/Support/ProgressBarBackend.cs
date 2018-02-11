1f18af8d4600937e12a464553089e93a6be9792f
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2018-02-11 15:48:24 +0100

    Added Progress bar backend helper class

---
+++ Logic/Support/ProgressBarBackend.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Constraints;
+using Swarmops.Logic.Cache;
+
+namespace Swarmops.Logic.Support
+{
+    public class ProgressBarBackend
+    {
+        public ProgressBarBackend(string guid)
+        {
+            this.Guid = guid;
+        }
+
+        public void Set (int progress)
+        {
+            GuidCache.Set(this.Guid + "-Progress", progress);
+
+        }
+
+        public string Guid { get; private set; }
+    }
+}
