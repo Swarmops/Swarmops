f552835c487cef4ef8f06adae0c6c5729b91beb5
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2018-03-05 18:38:04 +0100

    Sanitized JSON twice, leading to all sorts of problems

--- Site/Pages/v5/Ledgers/Json-GeneralLedgerData.aspx.cs
+++ Site/Pages/v5/Ledgers/Json-GeneralLedgerData.aspx.cs
@@ -116,8 +116,8 @@ namespace Swarmops.Frontend.Pages.v5.Ledgers
                     string description = transaction.Description;
 
                     string hasDoxString =
-                        "<img src='/Images/Icons/iconshock-search-256px.png' onmouseover=\\\"this.src='/Images/Icons/iconshock-search-hot-256px.png';\\\" onmouseout=\\\"this.src='/Images/Icons/iconshock-search-256px.png';\\\" data-txid='{0}' class='LocalViewDox' style='cursor:pointer' height='20' width='20' />" +
-                        "<img src='/Images/Icons/iconshock-download-240px.png' onmouseover=\\\"this.src='/Images/Icons/iconshock-download-hot-240px.png';\\\" onmouseout=\\\"this.src='/Images/Icons/iconshock-download-240px.png';\\\" data-docid='{1}' data-docname=\\\"{2}\\\" class='LocalDownloadDox' style='cursor:pointer' height='18' width='18' />";
+                        "<img src='/Images/Icons/iconshock-search-256px.png' onmouseover=\"this.src='/Images/Icons/iconshock-search-hot-256px.png';\" onmouseout=\"this.src='/Images/Icons/iconshock-search-256px.png';\" data-txid='{0}' class='LocalViewDox' style='cursor:pointer' height='20' width='20' />" +
+                        "<img src='/Images/Icons/iconshock-download-240px.png' onmouseover=\"this.src='/Images/Icons/iconshock-download-hot-240px.png';\" onmouseout=\"this.src='/Images/Icons/iconshock-download-240px.png';\" data-docid='{1}' data-docname=\"{2}\" class='LocalDownloadDox' style='cursor:pointer' height='18' width='18' />";
 
                     string actionHtml = string.Empty;
 
