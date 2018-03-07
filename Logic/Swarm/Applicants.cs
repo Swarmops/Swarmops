fe15df3d11c89d712823ff0ed17c520668564caa
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2018-03-07 20:05:31 +0100

    Added Applicants to Logic

---
+++ Logic/Swarm/Applicants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Swarmops.Basic.Types.Swarm;
+using Swarmops.Logic.Support;
+
+namespace Swarmops.Logic.Swarm
+{
+    public class Applicants: PluralBase<Applicants, Applicant, BasicApplicant>
+    {
+    }
+}
