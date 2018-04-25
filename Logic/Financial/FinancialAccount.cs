83f51abbcfe9a8e72424ae98cc93df433bc76c81
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2018-04-25 17:10:12 +0200

    Defensive programming pays off

--- Logic/Financial/FinancialAccount.cs
+++ Logic/Financial/FinancialAccount.cs
@@ -194,7 +194,7 @@ namespace Swarmops.Logic.Financial
                 ExternalBankDataProfile oldProfile = ExternalBankDataProfileOldHack;
                 ExternalBankDataProfile newProfile = AutomationProfile.BankDataProfile;
 
-                if (oldProfile != null && newProfile != oldProfile)
+                if (oldProfile != null && newProfile.Identity != oldProfile.Identity)
                 {
                     throw new InvalidOperationException("Defensive programming for crucial part");                    
                 }
