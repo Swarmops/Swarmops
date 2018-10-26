dc86753f09c6d9a03d43376d23ff384b3918d8ca
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2018-10-26 13:01:53 +0200

    Adding some UI test stubs

---
+++ Logic/UITests/IUITest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swarmops.Logic.UITests
+{
+    public interface IUITest
+    {
+        string Id { get; }
+        string Name { get; }
+        void ExecuteServerSide();
+        string JavaScriptClientCode { get; }
+    }
+
+    public interface IUITestGroup
+    {
+        string GroupId { get; }
+        string GroupName { get; } 
+        string JavaScriptClientCodeDocReady { get; }
+        void InitializeServerSide();
+        IUITest[] Tests { get; }
+    }
+}
---
+++ Logic/UITests/Sockets.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swarmops.Logic.UITests
+{
+    public class Sockets: IUITestGroup
+    {
+        public string GroupId { get { return "Sockets"; } }
+
+        public string GroupName { get { return "Sockets"; } }
+
+        public void InitializeServerSide() { }
+
+        public string JavaScriptClientCodeDocReady { get { return string.Empty; } }
+        public IUITest[] Tests
+        {
+            get
+            {
+                return new IUITest[ ] {
+                    new TestSocketConnecting() /*,
+                    new TestSocketEcho(),
+                    new TestFrontendHeartbeatReceived(),
+                    new TestBackendHeartbeatReceived()*/
+                    };
+            }
+        }
+    }
+
+    public class TestSocketConnecting : IUITest
+    {
+        public string Id { get { return @"Browser";  } }
+
+        public string Name { get { return @"Browser WebSocket connection"; } }
+
+        public void ExecuteServerSide()
+        {
+            
+        }
+
+        public string JavaScriptClientCode { get { return string.Empty; } }
+    }
+}
