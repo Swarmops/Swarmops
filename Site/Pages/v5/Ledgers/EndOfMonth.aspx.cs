79c96fcd71c37536981e1cfcfed3dc5bc5e9cc2b
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2018-11-14 16:06:00 +0100

    Forgot the security requirement

--- Site/Pages/v5/Ledgers/EndOfMonth.aspx.cs
+++ Site/Pages/v5/Ledgers/EndOfMonth.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Swarmops.Logic.Security;
 
 namespace Swarmops.Frontend.Pages.Ledgers
 {
@@ -11,7 +12,7 @@ namespace Swarmops.Frontend.Pages.Ledgers
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            this.PageAccessRequired = new Access(this.CurrentOrganization, AccessAspect.BookkeepingDetails);
         }
 
 
