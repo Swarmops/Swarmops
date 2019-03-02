5a2ab1c689066d8912e7c14911b1d9ecdfe9ebbf
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2019-03-02 20:05:00 +0100

    Loc update: Serbian code fixes

--- BuildFiles/Localization/download.sh
+++ BuildFiles/Localization/download.sh
@@ -13,15 +13,15 @@ unzip activizr.zip
 
 echo "Renaming Serbian (Latin, Cyrillic) to .Net standard..."
 
-rename 's/\.sr\-SP\./\.sr\-Cyrl-RS\./' Logic/sr-SP/*
-rename 's/\.sr\-CS\./\.sr\-Latn-RS\./' Logic/sr-CS/*
+rename 's/\.sr\-SP\./\.sr\-Cyrl\-RS\./' Logic/*
+rename 's/\.sr\-CS\./\.sr\-Latn\-RS\./' Logic/*
 
-rename 's/\.sr\-SP\./\.sr\-Cyrl-RS\./' Site/sr-SP/*
-rename 's/\.sr\-SP\./\.sr\-Cyrl-RS\./' Site/sr-CS/*
+rename 's/\.sr\-SP\./\.sr\-Cyrl\-RS\./' Site/sr-SP/*
+rename 's/\.sr\-CS\./\.sr\-Latn\-RS\./' Site/sr-CS/*
 mv Site/sr-SP Site/sr-Cyrl-RS
 mv Site/sr-CS Site/sr-Latn-RS
 
-echo "Deploying..."
+#echo "Deploying..."
 
 cp -r Logic/* ../../Logic/Resources/
 cp -r Site/* ../../Site/App_GlobalResources/
