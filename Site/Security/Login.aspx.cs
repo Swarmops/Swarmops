34410a6be3411d6ae017de5b25550dd48f9635fb
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2021-02-16 14:14:03 +0100

    Works beter wit speling right

--- Site/Security/Login.aspx.cs
+++ Site/Security/Login.aspx.cs
@@ -538,7 +538,7 @@ namespace Swarmops.Pages.Security
         {
             get
             {
-                return new ResourceManager("Swarmops.Frontend.Pages.Security", Assembly.LoadFrom("/usr/share/swarmops/frontend/bin/Swarmops.Frontend.Pages.Security.dll"));
+                return new ResourceManager("Swarmops.Frontend.Pages.Security", Assembly.LoadFrom("/usr/share/swarmops/frontend/bin/Swarmops.Frontend.Pages.Security.resources.dll"));
             }
         }
 
