9c06c6cca8bf0f889ce2177145ec1ba3604a0175
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2021-06-28 18:14:39 +0200

    Still resolving version deps

--- Logic/Support/ProgressBarBackend.cs
+++ Logic/Support/ProgressBarBackend.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NUnit.Framework.Constraints;
 using Swarmops.Logic.Cache;
 using Swarmops.Logic.Support.SocketMessages;
 
