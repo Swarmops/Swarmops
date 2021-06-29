a870b42fc8b5f72ae48d2288d56475065c8794e2
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2021-06-29 09:04:19 +0200

    Testing more

---
+++ Site/Pages/v5/Public/DummyPageWithResources.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" Inherits="Swarmops.Frontend.Pages.v5.Public.DummyPageWithResources" CodeFile="DummyPageWithResources.aspx.cs"  %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <p><asp:Label runat="server" ID="LabelTest"></asp:Label></p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Site/Pages/v5/Public/DummyPageWithResources.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Resources;
+
+namespace Swarmops.Frontend.Pages.v5.Public
+{
+    public partial class DummyPageWithResources : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.LabelTest.Text = Resources.Global.Dashboard_Main_Temporary;
+        }
+    }
+}
