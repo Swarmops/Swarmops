89d25c09bbdca8b726a6cd4691fcc771d263049b
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2021-07-04 17:49:57 +0200

    Moving Loc to separate assy

---
+++ Localization/LocDomain.cs
@@ -0,0 +1,24 @@
+﻿namespace Swarmops.Localization
+{
+
+    public enum LocDomain
+    {
+        Unknown = 0,
+
+        Global,
+        Menu5,
+
+        PagesAdmin,
+        PagesComms,
+        PagesFinancial,
+        PagesGovernance,
+        PagesLedgers,
+        PagesPublic,
+        PagesSecurity,
+        PagesSwarm,
+
+        ControlsFinancial,
+        ControlsSwarm
+    };
+
+}
--- Localization/LocStrings.cs
+++ Localization/LocStrings.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Localization
+namespace Swarmops.Localization
 {
-    public class Class1
+    class LocStrings
     {
     }
 }
