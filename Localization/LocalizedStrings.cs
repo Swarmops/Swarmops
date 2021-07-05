36aee4607b69ed9c7ec8f5d48d072f532cced71e
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2021-07-05 10:37:02 +0200

    Added remaining loc domains

--- Localization/LocalizedStrings.cs
+++ Localization/LocalizedStrings.cs
@@ -18,23 +18,39 @@ namespace Swarmops.Localization
                 case LocDomain.Menu5:
                     return Menu5.ResourceManager.GetString(stringResource);
 
+                case LocDomain.PagesAdmin:
+                    return Pages_Admin.ResourceManager.GetString(stringResource);
+
+                case LocDomain.PagesComms:
+                    return Pages_Comms.ResourceManager.GetString(stringResource);
+
                 case LocDomain.PagesFinancial:
                     return Pages_Financial.ResourceManager.GetString(stringResource);
 
+                case LocDomain.PagesGovernance:
+                    return Pages_Governance.ResourceManager.GetString(stringResource);
+                
                 case LocDomain.PagesLedgers:
                     return Pages_Ledgers.ResourceManager.GetString(stringResource);
 
+                case LocDomain.PagesPublic:
+                    return Pages_Public.ResourceManager.GetString(stringResource);
+
+                case LocDomain.PagesSecurity:
+                    return Pages_Security.ResourceManager.GetString(stringResource);
+
+                case LocDomain.PagesSwarm:
+                    return Pages_Swarm.ResourceManager.GetString(stringResource);
+
                 case LocDomain.ControlsFinancial:
                     return Controls_Financial.ResourceManager.GetString(stringResource);
+
+                case LocDomain.ControlsSwarm:
+                    return Controls_Swarm.ResourceManager.GetString(stringResource);
 
                 default:
                     throw new NotImplementedException("Unimplemented localization domain: " + domain.ToString());
             }
         }
     }
-
-    public enum LocalizedString
-    {
-        Unknown = 0
-    };
 }
