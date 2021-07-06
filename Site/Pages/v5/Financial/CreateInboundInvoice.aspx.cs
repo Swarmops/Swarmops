f5649583587e5f3628419322e7ca27a97a3abf0c
Rick Falkvinge <rick.falkvinge@pirateacademy.eu>
2021-07-06 15:15:16 +0200

    We can remove the stale reference

--- Site/Pages/v5/Financial/CreateInboundInvoice.aspx.cs
+++ Site/Pages/v5/Financial/CreateInboundInvoice.aspx.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Globalization;
 using System.Web;
-using Resources;
 using Swarmops.Localization;
 using Swarmops.Logic.Financial;
 using Swarmops.Logic.Security;
